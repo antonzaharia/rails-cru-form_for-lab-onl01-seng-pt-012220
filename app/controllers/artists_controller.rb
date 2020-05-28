bc5669ea97d5ca8eae1f4dfc054ba18633a62141
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-05-28 17:56:09 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,6 +8,10 @@ class ArtistsController < ApplicationController
     @artist = Artist.new
   end
 
+  def create
+    @artist = Artist.create(post_params)
+  end
+
   private
     def post_params
       params.require(:artist).permit(:name, :bio)
