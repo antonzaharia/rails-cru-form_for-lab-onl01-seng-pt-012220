8fba71c89002c1cc38abb3ba4e8c61747f2cecbd
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-05-28 18:12:57 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,6 +20,10 @@ class SongsController < ApplicationController
 
   def edit
     @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
     @song.update(params.require(:song).permit(:name, :artist_id, :genre_id))
     redirect_to song_path(@song)
   end
